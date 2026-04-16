d1920f2657b08a3501ed5a4ec108694874d87b40
NicoleLan <167232993+NicoleLan@users.noreply.github.com>
2026-04-16 10:59:34 -0300

    Mas funcionalidades

--- Tp2/funciones_Landet.py
+++ Tp2/funciones_Landet.py
@@ -1,6 +1,55 @@
 def suma (a, b):
     return a + b
 
-
 def statistics (a, b):
     return a/b
+
+def greet():
+    print ("¡Hola!")
+    return str(input("¿Cuál es tu nombre, capitán?"))
+
+
+def getData():
+    cargaBase = int(input("¿Cuánto es la carga base?"))
+    reserva = int(input("¿Cuánto es la reserva?"))
+    return cargaBase, reserva
+
+def checkFuel(reserva, cargaBase):
+    if reserva < 50:
+        reserva = 50 
+        print ("La reserva fue reescrita a 50u por razones de seguridad.")
+        resultado = suma (cargaBase, reserva)
+    else:
+        resultado = suma (cargaBase, reserva)
+        print (resultado)
+    return resultado
+
+def checkComponents():
+    cantComponentes = int(input("Cuántos componentes vas a checkear?"))
+    operativo = 0
+    fallido = 0
+    while (cantComponentes > 0):
+        nombreComponente = str(input("Ingrese nombre del componente:")) 
+        nivelEnergia = int(input("¿Qué nivel de energía tiene? (0-100)"))
+        funciona = checkComponent(nombreComponente, nivelEnergia)
+        if (funciona) {
+            operativo += 1
+        } else {
+            fallido += 1
+        }
+        cantComponentes -= 1
+    return operativo, fallido
+
+
+def checkComponent(nombre, energia):
+    if nivelEnergia >= 70:
+        print ("OPERATIVO")
+        return true
+
+    else:
+        print ("FALLIDO")
+        return false
+
+
+    
+
